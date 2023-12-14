d36007a38484e83c6914342e32920f295d4e104e
asib ahmed <ahmedasib1@gmail.com>
2023-12-14 23:20:33 +0600

    booking delete complete

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -11,7 +11,25 @@ const Bookings = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, [])
+    }, []);
+
+    const handleDelete = id =>{
+        const processed = confirm('are you sure want to delete?');
+        if (processed){
+            fetch(`http://localhost:5000/bookings/${id}`,{
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data);
+                if(data.deletedCount> 0){
+                    alert('deleted successfully');
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    setBookings(remaining);
+                }
+            })
+        }
+    }
 
     return (
         <div className="overflow-x-auto">
@@ -37,7 +55,7 @@ const Bookings = () => {
                 <tbody>
                     {/* row 1 */}
                    {
-                    bookings.map(booking => <BookingRow key={booking._id} booking={booking}></BookingRow>)
+                    bookings.map(booking => <BookingRow key={booking._id} booking={booking} handleDelete={handleDelete}></BookingRow>)
                    }
                 </tbody>
 
